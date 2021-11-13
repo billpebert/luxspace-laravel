610f50c7ad82eb6d788a33b8a80a423b22cc0361
Billy Ponto <bpgponto@gmail.com>
2021-11-13 09:02:33 +0800

    model relation

--- app/Models/ProductGallery.php
+++ app/Models/ProductGallery.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductGallery extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'products_id', 'url', 'is_featured'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
 }
--- app/Models/TransactionItem.php
+++ app/Models/TransactionItem.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'users_id', 'products_id', 'transactions_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
+
+    public function product()
+    {
+        return $this->hasOne(Product::class, 'products_id', 'id');
+    }
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transactions_id', 'id');
+    }
 }

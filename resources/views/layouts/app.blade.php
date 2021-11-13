06538b7f78b3267f06b0c5f2028d80cd2efaff9f
Billy Ponto <bpgponto@gmail.com>
2021-11-13 16:57:34 +0800

    product datatable & create product

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,9 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    {{--
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}"> --}}
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
     <!--- Tambahkan setelah app.css --->
     <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,9 +29,10 @@ Route::get('/checkout/success', [FrontendController::class, 'success'])->name('c
 //     return view('dashboard');
 // })->name('dashboard');
 
-Route::middleware(['auth:sanctun', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function () {
-    Route::get('/', [DashboardController::class], 'index')->name('index');
+Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
 
     Route::middleware(['admin'])->group(function () {
+        Route::resource('product', ProductController::class);
     });
 });
